33f398d6501b9c18ab99ded592bc0820456d9de8
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2021-02-03 23:49:27 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import Navitem from './Navitem';
+
+class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={'NavItemActive':''}
+    }
+
+    activeitem=(x) => {
+        if (this.state.NavItemId.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({'NavItemActive':x}, () => {
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+
+    };
+
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
+                    <Navitem item="About" tolink="/" activec={this.activeitem}></Navitem>
+                    <Navitem item="Skills" tolink="/" activec={this.activeitem}></Navitem>
+                    <Navitem item="Education" tolink="/" activec={this.activeitem}></Navitem>
+                    <Navitem item="Contact" tolink="/" activec={this.activeitem}></Navitem>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar

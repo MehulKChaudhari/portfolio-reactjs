c56e57ad04c2c1b8a58a42aafe45350ed863d086
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2021-02-12 15:37:25 +0530

    feat:Added blogs section

---
+++ src/contents/Blogs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Social from '../Components/Social';
+
+class Blogs extends Component {
+    render() {
+        return (
+            <div className="condiv Skills">
+                <h2> Algorithm-A deep dive</h2>
+                <a href="https://blog.codestrike.in/algorithms-a-deep-dive">Check out blog here!</a>
+                <div>
+                    <Social />
+                </div>
+                
+            </div>
+        )   
+    }
+}
+export default Blogs
